38b60fd51a0c679ed520fd9a92f517c49574d241
Adriano Santin <adr.santin@gmail.com>
2020-01-16 14:38:43 -0300

    Inject utils class on service

--- src/main/java/br/com/adriano/service/impl/UserServiceImpl.java
+++ src/main/java/br/com/adriano/service/impl/UserServiceImpl.java
@@ -3,16 +3,25 @@ package br.com.adriano.service.impl;
 import br.com.adriano.service.UserService;
 import br.com.adriano.ui.model.request.UserDetailsModelRequest;
 import br.com.adriano.ui.model.response.UserRest;
+import br.com.adriano.utils.Utils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     Map<String, UserRest> users;
+    Utils utils;
+
+    public UserServiceImpl() {}
+
+    @Autowired
+    public UserServiceImpl(Utils utils) {
+        this.utils = utils;
+    }
 
     @Override
     public UserRest createUser(UserDetailsModelRequest userDetails) {
@@ -21,7 +30,7 @@ public class UserServiceImpl implements UserService {
         returnValue.setLastName(userDetails.getLastName());
         returnValue.setEmail(userDetails.getEmail());
 
-        String userId = UUID.randomUUID().toString();
+        String userId = utils.generateUserId();
         returnValue.setUserId(userId);
 
         if (users == null) users = new HashMap<>();
---
+++ src/main/java/br/com/adriano/utils/Utils.java
@@ -0,0 +1,13 @@
+package br.com.adriano.utils;
+
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class Utils {
+
+    public String generateUserId() {
+        return UUID.randomUUID().toString();
+    }
+}
